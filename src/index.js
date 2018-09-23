b533722e2b31d76f01deb5f734a84f9bf8a0912c
Thomas Bußmeyer <thomas.bussmeyer@publicispixelpark.de>
2018-09-23 20:19:06 +0200

    add images via template.

--- src/index.js
+++ src/index.js
@@ -5,12 +5,3 @@ import 'tippy.js/dist/tippy.css';
 import tippy from 'tippy.js';
 
 const responsiveImage = require(`./${process.env.IMAGE}`);
-
-var h1 = document.createElement('h1');
-h1.innerHTML = 'Hello world!';
-document.body.appendChild(h1);
-
-var img = new Image();
-img.src = responsiveImage.src;
-img.setAttribute('srcset', responsiveImage.srcSet);
-document.body.appendChild(img);
