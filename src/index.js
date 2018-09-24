d438140d575a5c916edb5595a49836576665b846
Thomas Bußmeyer <thomas.bussmeyer@publicispixelpark.de>
2018-09-24 15:46:21 +0200

    Fix some lighthouse performance issues.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import 'normalize.css';
 import './style.css';
 import 'tippy.js/dist/tippy.css';
 
-import tippy from 'tippy.js';
+import tippy from 'tippy.js/dist/tippy.min.js';
 import loadFullImages from './loadImages';
 import ResponsiveBackgroundImage from './ResponsiveBackgroundImage'
 
