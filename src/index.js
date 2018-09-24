6c036cf906631a851bc15b383c6d50bceadcd246
Thomas Bußmeyer <thomas.bussmeyer@publicispixelpark.de>
2018-09-24 15:07:34 +0200

    Try responsive background image via css.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,14 @@ import 'tippy.js/dist/tippy.css';
 
 import tippy from 'tippy.js';
 import loadFullImages from './loadImages';
+import ResponsiveBackgroundImage from './ResponsiveBackgroundImage'
 
 window.addEventListener('load', function onWindowLoad() {
   loadFullImages();
 });
+
+let elements = document.querySelectorAll('[data-responsive-background-image]');
+
+for (let i=0; i<elements.length; i++) {
+  new ResponsiveBackgroundImage(elements[i]);
+}
